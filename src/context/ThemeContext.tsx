46361f66ddf71cf7254d35ade66337f53212d0df
Mahesh <deshmukhmahesh009@gmail.com>
2022-04-29 00:59:22 +0530

    feat: theme context created

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+import { ReactChildren, ThemeContextType } from "../types";
+
+const ThemeContext = createContext({} as ThemeContextType);
+
+const ThemeProvider = ({ children }: ReactChildren) => {
+  const [currentTheme, setCurrentTheme] = useState(
+    JSON.parse(localStorage.getItem("orion-quiz-theme") || "") || "dark"
+  );
+  return (
+    <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
