c439b1a024a42ca01f34cdb3da84a8c04a13b75d
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-01 03:00:52 +0530

    feat: firebase authentication implemented

--- src/pages/authentication/Signin.tsx
+++ src/pages/authentication/Signin.tsx
@@ -1,31 +1,55 @@
 import "./authentication.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Input } from "../../components";
-import { validLoginFormChecker } from "./utils";
+import { Input, Loading, OverlayContainer, Navbar } from "../../components";
 import { PasswordInput } from "../../components/input/PasswordInput";
-import { Navbar } from "../../components";
-import { FormErrorsType, ReactChangeEvent, ReactMouseEvent } from "../../types";
+import { FormErrorsType, ReactChangeEvent, ReactMouseEvent, LocationState } from "../../types";
+import { validLoginFormChecker } from "./utils";
+import { useAuth } from "../../hooks";
 
 const Signin = () => {
+  const [formErrors, setFormErrors] = useState({} as FormErrorsType);
+  const [submitted, setSubmitted] = useState(false);
   const [userInput, setUserInput] = useState({
     email: "",
     password: "",
   });
-  const [formErrors, setFormErrors] = useState({} as FormErrorsType);
-  const [submitted, setSubmitted] = useState(false);
+
+  const {
+    login,
+    authState: { isLoading, error },
+  } = useAuth();
+
+  const location = useLocation();
+
+  const { from } = (location.state as LocationState) || { from: { pathname: "/" } };
 
   const changeHandler = (e: ReactChangeEvent) => {
     const { name, value } = e.target;
     setUserInput({ ...userInput, [name]: value });
   };
+
   const formSubmitHandler = (e: ReactMouseEvent) => {
     e.preventDefault();
     setSubmitted(true);
+    if (!(Object.values(formErrors).length > 0)) {
+      login(userInput, from);
+      setFormErrors({});
+    }
   };
   useEffect(() => {
     setFormErrors(() => validLoginFormChecker(userInput));
   }, [userInput, submitted]);
+
+  const loginWithGuest = (e: ReactMouseEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+    setUserInput({
+      email: "quizmaster@orion.com",
+      password: "Mahesh@1906",
+    });
+    login({ email: "quizmaster@orion.com", password: "Mahesh@1906" }, from);
+  };
 
   return (
     <div className="form-wrapper">
@@ -33,6 +57,13 @@ const Signin = () => {
       <div className="form-container flex-total-center">
         <form className="form-grp">
           <h2 className="text-center text-lg">Signin</h2>
+
+          {error && <p className="text-danger text-center">{error}</p>}
+
+          <OverlayContainer display={isLoading}>
+            <Loading />
+          </OverlayContainer>
+
           <Input
             type="Email"
             required={true}
@@ -66,7 +97,7 @@ const Signin = () => {
           <button className="btn btn-primary btn-submit" onClick={e => formSubmitHandler(e)}>
             Login
           </button>
-          <button className="btn btn-outline-primary btn-submit">
+          <button className="btn btn-outline-primary btn-submit" onClick={e => loginWithGuest(e)}>
             Login with test credentials
           </button>
 
