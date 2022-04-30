b3b18c46a4a600eacc4c6a4a67ee14618b183c8f
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-01 02:57:34 +0530

    feat: loading wheel created

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,10 @@
+import "./loading.css";
+const Loading = () => {
+  return (
+    <div className="flex-total-center loading-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export { Loading };
