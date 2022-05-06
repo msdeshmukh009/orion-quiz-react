c45452a75cc1f343dbeb9e9c2d89fdeeafde9bf7
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-07 03:06:42 +0530

    refactor: extracted hook

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,3 +1,3 @@
 export { ThemeContext, ThemeProvider } from "./ThemeContext";
 export { AuthContext, AuthProvider } from "./AuthContext";
-export { useQuiz, QuizProvider } from "./QuizContext";
+export { QuizContext, QuizProvider } from "./QuizContext";
