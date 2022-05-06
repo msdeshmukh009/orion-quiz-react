2d6b46b8ba2b7134be499133e92c17fa20ff653d
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-07 02:55:07 +0530

    feat: Toaster component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useTheme } from "./hooks";
 import { NavigationRoutes } from "./routes/NavigationRoutes";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { currentTheme } = useTheme();
 
   return (
     <div className={`${currentTheme === "dark" ? "dark" : "light"}`}>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 2000,
+          style: { background: "var(--background-color)", color: "var(--text-color)" },
+        }}
+      />
       <NavigationRoutes />
     </div>
   );
