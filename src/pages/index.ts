b6ec4c791243362302c43142b43ef7b3912261c4
Mahesh <deshmukhmahesh009@gmail.com>
2022-05-07 03:08:07 +0530

    refactor: create quiz exported

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,3 +5,4 @@ export { QuestionPage } from "./question-page/QuestionPage";
 export { Result } from "./result/Result";
 export { Signin, Signup } from "./authentication";
 export { Profile } from "./profile/Profile";
+export { CreateQuiz } from "./create-quiz/CreateQuiz";
